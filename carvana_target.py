7023e99d6a7c619c0a64681c8e28ff53bb2a588f
Eric Fowler <eric.fowler@gmail.com>
2017-11-20 15:54:45 -0800

    Add test phase

--- carvana_target.py
+++ carvana_target.py
@@ -12,10 +12,10 @@ class CarvanaTarget(Target):
         self.crop_images = crop_images
         self.img_shape = ut.get_image_shape(self.sample, crop=self.crop_images) if num_pixels == None else num_pixels
 
-    def generator(self, tensors, path):
+    def generator(self, tensors, path, crop):
         ret = []
         for fn, l in tensors:
-            img = ut.read_image(path=path, fname=fn, show=False, scale =1.0, crop=self.crop_images)
+            img = ut.read_image(path=path, fname=fn, show=False, scale =1.0, crop=crop)
             #print('One image:'+fn)
             ret.append((img,l))
         return ret
@@ -105,5 +105,22 @@ class CarvanaTarget(Target):
         global_step = tf.Variable(0, name='global_step', trainable=False)
         train_op = optimizer.minimize(loss, global_step=global_step)
         return train_op
+
+    def do_eval(self, sess, eval_op, pl_imgs, pl_labels, tensor_list, batch_size, data_path, crop):
+        true_count = 0
+        steps_per_epoch = len(tensor_list) // batch_size
+        num_examples = steps_per_epoch * batch_size
+        for step in range(steps_per_epoch):
+            for tensors in ut.grouper(tensor_list, batch_size):
+                if tensors == None:
+                    break
+                tensor_batch=self.generator(tensors, path=data_path, crop=crop)
+                imgs = [tupl[0] for tupl in tensor_batch]
+                labels = [tupl[1] for tupl in tensor_batch]
+                true_count + sess.run(eval_op, feed_dict = {pl_imgs:imgs, pl_labels:labels})
+
+            precision = float(true_count)/num_examples
+            print('Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
+                  (num_examples, true_count, precision))
 
 
--- estimator.py
+++ estimator.py
@@ -7,10 +7,10 @@
 
 # <editor-fold desc="Main">
 import utilities as ut
-from target import Target
 from carvana_target import CarvanaTarget
 import tensorflow as tf
-
+import time
+import os
 
 def main():
     # <editor-fold desc="Process parameters into globals">
@@ -24,7 +24,6 @@ def main():
     else:
         return
 
-
 # set up graph
     with tf.Graph().as_default():
         (image_placeholder, label_placeholder) = target.get_graph_placeholders(img_shape=IMG_SHAPE, batch_size=FLAGS.batch_size)
@@ -32,25 +31,75 @@ def main():
         evaluation_op = target.evaluation(logits=logits_op, labels=label_placeholder)
         loss_op = target.loss(logits=logits_op, labels=label_placeholder)
         train_op = target.training(learning_rate=FLAGS.learning_rate, loss=loss_op)
+
+        # Create a saver for writing training checkpoints.
+        saver = tf.train.Saver()
+
         sess = tf.InteractiveSession(config = tf.ConfigProto(log_device_placement=True))
+
+        # Build the summary Tensor based on the TF collection of Summaries.
+        summary = tf.summary.merge_all()
+
+        # Instantiate a SummaryWriter to output summaries and the Graph.
+        summary_writer = tf.summary.FileWriter(FLAGS.tb_dir, sess.graph)
+
         gvi=tf.global_variables_initializer()
         gvi.run(session=sess)
-
+        start_time = time.time()
 
 #chop list into chunx, process each one
         for epoch in range(FLAGS.epochs):
+            epoch_start_time = time.time()
             # get tensor lists
             # get train, test, validate(?) lists
             tensor_list_train = target.get_tensor_list(path=FLAGS.train_data_path)
             tensor_list_test = target.get_tensor_list(path=FLAGS.test_data_path)
-
+            batch_num = 0
             for tensors in ut.grouper(tensor_list_train,FLAGS.batch_size):
-                tensor_batch=target.generator(tensors,path=FLAGS.train_data_path)
+                batch_start_time = time.time()
+                if tensors == None:
+                    break
+                tensor_batch=target.generator(tensors,path=FLAGS.train_data_path, crop=FLAGS.crop)
                 imgs = [tupl[0] for tupl in tensor_batch]
                 labels = [tupl[1] for tupl in tensor_batch]
 
                 _, loss=sess.run([train_op, loss_op], feed_dict={image_placeholder:imgs, label_placeholder:labels})
-                print(loss)
+                print('Loss:%f, batch elapsed time %.3f, batch %d of %d'% (loss, time.time() - batch_start_time, batch_num, len(tensor_list_train)//FLAGS.batch_size))
+
+            epoch_total_time = time.time() - epoch_start_time
+            print('Epoch time:%.3f secs'% (epoch_total_time))
+            duration = time.time() - start_time
+            if epoch % 100 == 0:
+                # Print status to stdout.
+                print('Step %d: loss = %.2f (%.3f sec)' % (epoch, loss, duration))
+                # Update the events file.
+                summary_str = sess.run(summary, feed_dict={image_placeholder:imgs, label_placeholder:labels})
+                summary_writer.add_summary(summary_str, epoch)
+                summary_writer.flush()
+
+                if (epoch + 1) % 1000 == 0 or (epoch + 1) == FLAGS.epochs:
+                    print('Training Data Eval:')
+                    target.do_eval(sess=sess,
+                                   eval_op=evaluation_op,
+                                   pl_imgs=image_placeholder,
+                                   pl_labels=label_placeholder,
+                                   tensor_list=tensor_list_train,
+                                   batch_size=FLAGS.batch_size,
+                                   data_path=FLAGS.train_data_path,
+                                   crop=FLAGS.crop)
+                    checkpoint_file = os.path.join(FLAGS.log_dir, 'model.ckpt')
+                    saver.save(sess, checkpoint_file, global_step=epoch)
+                    # Evaluate against the training set.
+
+                    print('Test Data Eval:')
+                    target.do_eval(sess=sess,
+                                   eval_op=evaluation_op,
+                                   pl_imgs=image_placeholder,
+                                   pl_labels=label_placeholder,
+                                   tensor_list=tensor_list_test,
+                                   batch_size=FLAGS.batch_size,
+                                   data_path=FLAGS.test_data_path,
+                                   crop=FLAGS.crop)
 
     pass
 
