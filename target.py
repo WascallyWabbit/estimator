59efe7195fb7a218c9a6ee11c353f34abd8ca9b3
Eric Fowler <eric.fowler@gmail.com>
2017-11-24 18:10:29 -0800

    do_eval() in base class

--- target.py
+++ target.py
@@ -9,7 +9,6 @@ class Target:
         self.img_shape = ut.get_image_shape(self.sample, crop=self.crop_images,scale=scale) if num_pixels == None else num_pixels
 
 
-
     def generator(self, tensors, path, crop,scale):
         ret = []
         try:
@@ -23,3 +22,22 @@ class Target:
             return []
 
         return ret
+
+    def do_eval(self, sess, eval_op, pl_imgs, pl_labels, tensor_list, batch_size, data_path, crop, scale):
+        true_count = 0
+        steps_per_epoch = len(tensor_list) // batch_size
+        num_examples = steps_per_epoch * batch_size
+        for step in range(steps_per_epoch):
+            for tensors in ut.grouper(tensor_list, batch_size):
+                if tensors is None or len(tensors) < batch_size:
+                    break
+                tensor_batch = self.generator(tensors, path=data_path, crop=crop, scale=scale)
+                if len(tensor_batch) < batch_size:
+                    break
+                imgs = [tupl[0] for tupl in tensor_batch]
+                labels = [tupl[1] for tupl in tensor_batch]
+                true_count + sess.run(eval_op, feed_dict={pl_imgs: imgs, pl_labels: labels})
+
+            precision = float(true_count) / num_examples
+            print('Num examples: %d  Num correct: %d  Precision @ 1: %0.04f' %
+                  (num_examples, true_count, precision))
